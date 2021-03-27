a20b3176f8eca0afc989b0132c0790064e5f61ab
Jhonatan Edward <jhonatanedsp@gmail.com>
2021-03-27 02:35:04 -0300

    First controller model and view

---
+++ spring-demo/src/com/luv2code/springdemo/mvc/HelloWorldController.java
@@ -0,0 +1,37 @@
+package com.luv2code.springdemo.mvc;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloWorldController {
+	
+	@RequestMapping("/showForm")
+	public String showForm() {
+		return "helloworld-form";
+	}
+	
+	@RequestMapping("/processForm")
+	public String processForm() {
+		return "helloworld";
+	}
+	
+	// new a controller method to read from data and
+	// add data to de model
+	@RequestMapping("/processFormVersionTwo")
+	public String letsShoutDude(HttpServletRequest request, Model model) {
+		
+		String theName = request.getParameter("studentName");
+		
+		theName = theName.toUpperCase();
+		
+		String message = "Yo! " + theName;
+		
+		model.addAttribute("message", message);
+		
+		return "helloworld";
+	}
+}
